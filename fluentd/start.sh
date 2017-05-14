8d2e8de07f95695b75d2eeb0e5afbb3a64677a1e
Evan Lezar <evanlezar@gmail.com>
2017-05-14 12:26:29 +0200

    Add logfile output and comment out webhook

--- fluentd/start.sh
+++ fluentd/start.sh
@@ -6,9 +6,13 @@ source .env
 fi
 
 FLUENTD_HTTP_ENDPOINT=${FLUENTD_HTTP_ENDPOINT:-http://www.example.com}
+SPEEDTEST_LOG_PATH=${SPEEDTEST_LOG_PATH:-/tmp}
 
 IMAGE_NAME=fluentd-logger
-
+if [ ! -d ${SPEEDTEST_LOG_PATH} ]
+then
+  mkdir -p SPEEDTEST_LOG_PATH
+fi
 
 TIME_VOLUMES=
 if [ -e /etc/localtime ]
@@ -18,7 +22,9 @@ fi
 
 docker run --restart=always --name=fluentdapp -d \
         -e FLUENTD_OPT="-v" \
+        -e SPEEDTEST_LOG_PATH=${SPEEDTEST_LOG_PATH} \
         -e FLUENTD_HTTP_ENDPOINT=${FLUENTD_HTTP_ENDPOINT} \
+        -v ${SPEEDTEST_LOG_PATH}:/output_logs \
         ${TIME_VOLUMES} \
         -p 24224:24224 \
         ${IMAGE_NAME}
